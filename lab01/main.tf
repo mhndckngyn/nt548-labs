5b36081539ca645a594c4b2cfdf86cf3b4e801a4
ch1ta <ng.phihoc123@gmail.com>
2025-05-03 14:46:55 +0700

    add security groups module

--- lab01/main.tf
+++ lab01/main.tf
@@ -11,3 +11,28 @@ module "vpc" {
   private_subnet_cidrs = ["10.0.3.0/24", "10.0.4.0/24"]
   azs                  = ["us-east-1a", "us-east-1a"]
 }
+
+module "security_group" {
+  source = "./modules/security_group"
+
+  name   = "lab01"
+  vpc_id = module.vpc.vpc_id
+}
+
+module "ec2" {
+  source = "./modules/ec2"
+
+  project_name           = "lab01"
+  instance_type          = "t2.micro"
+  ami_id                 = ""
+  
+  # Public instance
+  public_subnet_id        = module.vpc.public_subnets[0]
+  public_security_group_id = module.security_group.public_security_group_id
+  create_public_instance  = true
+  
+  # Private instance
+  private_subnet_id        = module.vpc.private_subnets[0]
+  private_security_group_id = module.security_group.private_security_group_id
+  create_private_instance  = true
+}
---
+++ lab01/modules/security_groups/variables.tf
@@ -0,0 +1,9 @@
+variable "project_name" {
+  description = "lab01"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "ID of the VPC"
+  type        = string
+}
