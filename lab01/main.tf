412a435f64f830a75933ce60cc7ff9228cd35ef9
ch1ta <ng.phihoc123@gmail.com>
2025-05-04 16:29:21 +0700

    add provider for main file

--- lab01/main.tf
+++ lab01/main.tf
@@ -1,38 +1,31 @@
-provider "aws" {
-  region = "us-east-1"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 }
 
 module "vpc" {
   source = "./modules/vpc"
 
-  name                 = "lab01"
-  vpc_cidr             = "10.0.0.0/16"
-  public_subnet_cidrs  = ["10.0.1.0/24", "10.0.2.0/24"]
-  private_subnet_cidrs = ["10.0.3.0/24", "10.0.4.0/24"]
-  azs                  = ["us-east-1a", "us-east-1a"]
-}
-
-module "security_group" {
-  source = "./modules/security_group"
-
-  name   = "lab01"
-  vpc_id = module.vpc.vpc_id
+  vpc_cidr             = var.vpc_cidr
+  public_subnet_cidrs  = var.public_subnet_cidrs
+  private_subnet_cidrs = var.private_subnet_cidrs
+  azs                  = var.azs
 }
 
 module "ec2" {
   source = "./modules/ec2"
 
-  project_name           = "lab01"
-  instance_type          = "t2.micro"
-  ami_id                 = ""
-  
-  # Public instance
-  public_subnet_id        = module.vpc.public_subnets[0]
-  public_security_group_id = module.security_group.public_security_group_id
+  vpc_id                = module.vpc.vpc_id
+  public_subnet_id      = module.vpc.public_subnets[0]
+  private_subnet_id     = module.vpc.private_subnets[0]
+  ami_id                = var.ami_id
+  instance_type         = var.instance_type
+  key_name              = "lab01-keypair"
+  allowed_ssh_cidr      = var.allowed_ssh_cidr
   create_public_instance  = true
-  
-  # Private instance
-  private_subnet_id        = module.vpc.private_subnets[0]
-  private_security_group_id = module.security_group.private_security_group_id
-  create_private_instance  = true
+  create_private_instance = true
 }
