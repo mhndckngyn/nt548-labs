2bd1e38e1e9f0cd1977b16bdd90e6bf39b908f81
Nguyễn Phi Học <85166295+phihocnguyen@users.noreply.github.com>
2025-05-04 18:48:25 +0700

    Delete lab01 directory

--- lab01/modules/security_groups/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "public_security_group_id" {
-  description = "ID of the public security group"
-  value       = aws_security_group.public.id
-}
-
-output "private_security_group_id" {
-  description = "ID of the private security group"
-  value       = aws_security_group.private.id
-} 
