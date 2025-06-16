7ca1a53846507b196938d02ccafb07a03ab16d3e
mhndckngyn <126580562+mhndckngyn@users.noreply.github.com>
2025-06-16 19:51:53 +0700

    fix(lab02-bt1): update output values to handle no instances case

--- lab02/exercise1/modules/ec2/outputs.tf
+++ lab02/exercise1/modules/ec2/outputs.tf
@@ -1,11 +1,11 @@
 output "public_instance_id" {
   description = "ID of the public EC2 instance"
-  value       = aws_instance.public[0].id
+  value       = length(aws_instance.public) > 0 ? aws_instance.private[0].id : null
 }
 
 output "private_instance_id" {
   description = "ID of the private EC2 instance"
-  value       = aws_instance.private[0].id
+  value       = length(aws_instance.private) > 0 ? aws_instance.private[0].id : null
 }
 
 output "public_security_group_id" {
@@ -15,5 +15,5 @@ output "public_security_group_id" {
 
 output "private_security_group_id" {
   description = "ID of the private security group"
-  value       = aws_security_group.private.id
+  value       = length(aws_instance.private) > 0 ? aws_instance.private[0].id : null
 }
