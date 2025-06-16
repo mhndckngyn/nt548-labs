d86828cd5fa16eb773cf1a3cf4ef6180b041d585
mhndckngyn <126580562+mhndckngyn@users.noreply.github.com>
2025-06-16 19:34:00 +0700

    fix(lab02-bt1): remove aws credential variable blocks in terraform

--- lab02/exercise1/provider.tf
+++ lab02/exercise1/provider.tf
@@ -1,15 +1,3 @@
 provider "aws" {
   region = "ap-southeast-1"
 }
-
-variable "aws_access_key" {
-  description = "AWS Access Key"
-  type        = string
-  sensitive   = true
-}
-
-variable "aws_secret_key" {
-  description = "AWS Secret Key"
-  type        = string
-  sensitive   = true
-} 
